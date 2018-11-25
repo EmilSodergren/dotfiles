4bb25bcc54b6bbea618e08c143fda5eec358c005
Emil Södergren <EmilSodergren@users.noreply.github.com>
2018-11-25 10:55:18 +0100

    Make setup download Vundle plugins and more

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from os.path import dirname, realpath, expanduser, join, exists
 from os import symlink, remove
+from subprocess import call, Popen, PIPE
+from argparse import ArgumentParser
 
 dotfilespath = dirname(realpath(__file__))
 homefolder = expanduser("~")
 
 settingsfiles = [".vim", ".bashrc", ".tmux.conf", ".gitconfig", ".bash_git"]
+
+parser = ArgumentParser(description='Setup the machine')
+
+parser.add_argument('-i', '--internet', action='store_true', help='Download stuff from the internet')
+args = parser.parse_args()
 
 for stuff in settingsfiles:
     linkpath = join(homefolder, stuff)
@@ -13,4 +20,11 @@ for stuff in settingsfiles:
         remove(linkpath)
     symlink(sourcepath, linkpath)
 
-
+if args.internet:
+    call(["vim","+VundleInstall","+qall"])
+    call(["vim", "+GoInstallBinaries", "+qall"])
+    
+    ps = Popen(["curl", "https://sh.rustup.rs", "-sSf"], stdout=PIPE)
+    call(["sh"], stdin=ps.stdout)
+    ps.wait()
+    call(["rustup", "install", "nightly"])
