c769039602d5145f2c09f38b3f95c4ec8e7232a8
Emil Södergren <EmilSodergren@users.noreply.github.com>
2018-12-12 14:18:15 +0100

    Add bashcomplete and use rustfmt from stable

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from argparse import ArgumentParser
 dotfilespath = dirname(realpath(__file__))
 homefolder = expanduser("~")
 
-settingsfiles = [".vim", ".bashrc", ".tmux.conf", ".gitconfig", ".bash_git", ".profile"]
+settingsfiles = [".vim", ".bashrc", ".tmux.conf", ".gitconfig", ".bash_git", ".profile", ".bash_completion", ".bash_completion.d"]
 
 parser = ArgumentParser(description='Setup the machine')
 
 parser.add_argument('-i', '--internet', action='store_true', help='Download stuff from the internet')
+parser.add_argument('-n', '--nightly', action='store_true', help='Download the latest nightly build')
 args = parser.parse_args()
 
 for stuff in settingsfiles:
@@ -28,8 +29,10 @@ if args.internet:
     ps = Popen(["curl", "https://sh.rustup.rs", "-sSf"], stdout=PIPE)
     call(["sh", "-s", "--", "-y"], stdin=ps.stdout)
     ps.wait()
-    call(["rustup", "install", "nightly"])
-    call(["cargo", "+nightly", "install", "racer"])
+    call(["rustup", "component", "add", "rustfmt"])
     call(["rustup", "component", "add", "rust-src"])
-    call(["rustup", "component", "add", "rustfmt-preview", "--toolchain", "nightly"])
     call(["cargo", "install", "cargo-watch"])
+
+    if args.nightly:
+        call(["rustup", "install", "nightly"])
+        call(["cargo", "+nightly", "install", "racer"])
