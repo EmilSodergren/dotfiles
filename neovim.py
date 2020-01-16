04fee514db1224a083be86717a646f241893f25d
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-01-16 13:59:25 +0100

    Add uninstall target, workarund for release build
    I can not get the make variable to work properly so I change the
    Makefile in neovim folder bfore running make.

--- neovim.py
+++ neovim.py
@@ -2,6 +2,7 @@ from os.path import dirname, realpath, expanduser, join, exists, islink, basenam
 from os import symlink, remove, chdir, listdir, getcwd, remove, makedirs
 from subprocess import call, check_output, Popen, PIPE
 from argparse import ArgumentParser
+import fileinput
 import re
 
 homefolder = expanduser("~")
@@ -14,14 +15,19 @@ parser.add_argument('-o', '--online', action='store_true', help='Download stuff 
 parser.add_argument('-i', '--install', action='store_true', help='Build and install neovim')
 parser.add_argument('-c', '--clean', action='store_true', help='Clean before build and install')
 parser.add_argument('-p', '--pack', action='store_true', help='Pack neovim in a tar file')
+parser.add_argument('-u', '--uninstall', action='store_true', help='Uninstall neovim on the sudo make install path')
 args = parser.parse_args()
+
+if args.uninstall:
+    call(["sudo", "rm", "/usr/local/bin/nvim"])
+    call(["sudo", "rm", "-r", "/usr/local/share/nvim/"])
 
 chdir(homefolder)
 call(["sudo", "apt", "install", "-y", "python3-pip"])
 if args.clean or args.install:
     call(["sudo", "apt-get", "install", "ninja-build", "gettext", "libtool", "libtool-bin", "autoconf", "automake", "cmake", "g++", "pkg-config", "unzip"])
 
-    if args.clean:
+    if args.clean and exists(neovimdir):
         chdir(neovimdir)
         call(["sudo", "make", "distclean"])
         call(["sudo", "rm", "-rf", pynvim])
@@ -42,7 +48,11 @@ if args.online:
 
 if args.install:
     chdir(neovimdir)
-    call(["make", "CMAKE_BUILD_TYPE=RelWithDebInfo"])
+    # Can't get the make parameter CMAKE_BUILD_TYPE to take
+    # Change the Makefile instead, before continuing
+    for line in fileinput.input("Makefile", inplace=True):
+        print(line.replace("Debug", "Release"), end="")
+    call(["make"])
     call(["sudo", "make", "install"])
     chdir(pynvim)
     call(["python3", "-m", "pip", "install", "--user", "-f", "./"] + listdir(pynvim) + ["--no-index"])
