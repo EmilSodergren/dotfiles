d930abb7e5bb01128a6c8bd6e52188e392b607d4
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2022-09-20 20:37:43 +0200

    Add all my plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,7 @@
 vim.g.mapleader = ","
 
-local fn = vim.fn
-local homedir = os.getenv("HOME")
-local packerdir = homedir .. '/.local/share/nvim/site/pack/packer/start/packer.nvim/'
-
-if fn.empty(fn.glob(packerdir)) > 0 then
-  fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    packerdir,
-  }
-  vim.cmd [[packadd packer.nvim]]
-end
-vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+require('utils')
+init_packer()
 
 require('plugins')
 
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,37 @@
 return require('packer').startup(function(use)
 
+  use 'Raimondi/delimitMate'
+  use 'Shougo/deoplete-lsp'
+  use {'Shougo/deoplete.nvim', cmd = 'UpdateRemotePlugins' }
+  use 'Shougo/echodoc.vim'
+  use 'Shougo/neosnippet-snippets'
+  use 'Shougo/neosnippet.vim'
+  use 'airblade/vim-gitgutter'
+  use 'alec-gibson/nvim-tetris'
+  use 'cespare/vim-toml'
+  use 'cyberkov/openhab-vim'
+  use 'dhruvasagar/vim-zoom'
+  use {'elzr/vim-json', ft = 'json'}
+  use {'fatih/vim-go', cmd = 'GoUpdateBinaries'}
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && ./install.sh', ft = 'markdown'}
+  use 'jreybert/vimagit'
+  use {'junegunn/fzf', run = 'cd ~/.fzf && ./install --bin' }
+  use 'junegunn/fzf.vim'
+  use {'lepture/vim-jinja', ft = 'jinja'}
+  use 'machakann/vim-highlightedyank'
+  use 'mbbill/undotree'
+  use 'neovim/nvim-lspconfig'
+  use {'plasticboy/vim-markdown', ft = 'markdown'}
+  use {'rust-lang/rust.vim', ft = 'rust'}
+  use 'simeji/winresizer'
+  use 'simrat39/rust-tools.nvim'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-surround'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
   use 'wbthomason/packer.nvim'
+  use 'wellle/tmux-complete.vim'
 
 end)
 
