ce1cce02d30be75270ffe2f8ebc5956724e017c5
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2022-09-20 19:12:33 +0200

    Refactored init of packer.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,21 @@
 vim.g.mapleader = ","
 
 local fn = vim.fn
+local homedir = os.getenv("HOME")
+local packerdir = homedir .. '/.local/share/nvim/site/pack/packer/start/packer.nvim/'
 
-if fn.empty(fn.glob('~/.local/share/nvim/site/pack/packer/start/packer.nvim/')) > -1 then
+if fn.empty(fn.glob(packerdir)) > 0 then
   fn.system {
     "git",
     "clone",
     "--depth",
     "1",
     "https://github.com/wbthomason/packer.nvim",
-    "site/pack/packer/start/packer.nvim/",
+    packerdir,
   }
   vim.cmd [[packadd packer.nvim]]
 end
 vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
 
-require('packercfg')
+require('plugins')
 
