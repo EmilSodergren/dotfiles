5921a3d0a88229af0ca8bf26c039f4be7a02abfc
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2022-10-25 13:00:00 +0200

    fzf is no longer in home folder

--- setup.py
+++ setup.py
@@ -226,7 +226,7 @@ if args.pack or args.artifactory:
     chdir(homefolder)
     call([
         "tar", r"--exclude=*/\.git", "-czf", "dotfiles.tar.gz", ".dotfiles/", "go/bin/", ".cargo/bin/", ".cargo/env", local_bin,
-        ".local/node_modules", ".local/include", ".local/lib", ".local/share/nvim", ".local/share/konsole", ".fzf"
+        ".local/node_modules", ".local/include", ".local/lib", ".local/share/nvim", ".local/share/konsole"
     ])
     print("")
     print(".dotfiles has been packed into " + join(homefolder, "dotfiles.tar.gz"))
