bea5cff4ef83888700c3c38880464d41123a87c9
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2025-06-03 14:21:11 +0200

    Modify golangci_lint_ls config

--- .config/nvim/lua/plugins-config.lua
+++ .config/nvim/lua/plugins-config.lua
@@ -192,7 +192,7 @@ lspconfig.gopls.setup(coq.lsp_ensure_capabilities({
 }))
 lspconfig.golangci_lint_ls.setup(coq.lsp_ensure_capabilities({
   cmd = { home .. "/go/bin/golangci-lint-langserver" },
-  root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+  root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
   settings = {
     init_options = { command = { home .. "/go/bin/golangci-lint", "run", "--fast-only", "--output.json.path=stdout", "--show-stats=false" } }
   }
