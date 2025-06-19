4f8ab5605fb7cdd345e046b3ebd473bb78c6bf52
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2025-06-19 16:59:10 +0200

    HEAVY WIP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,8 @@
-vim.g.mapleader = ","
+require("config.lazy")
 
-require("utils")
-init_packer()
-
-require("plugins")
-require("plugins-config")
-require("color")
-require("options")
-require("keymaps")
-require("autocmd")
-
+-- require("plugins")
+-- require("plugins-config")
+-- require("plugins.color")
+-- require("options")
+-- require("plugins.keymaps")
+-- require("autocmd")
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/which-key.nvim",           enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim",           enabled = false },
+  { "folke/snacks.nvim",              opts = { indent = { enabled = false } } },
+}
