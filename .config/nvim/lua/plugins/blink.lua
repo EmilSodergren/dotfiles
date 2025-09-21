36ff187e511b8fab0a0a7ba2327aacd1f18b0aea
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2025-09-21 13:53:33 +0200

    Put blink sources in a defined order.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,22 +3,35 @@ return {
     "saghen/blink.cmp",
     dependencies = {
       "mgalliou/blink-cmp-tmux",
+      "rafamadriz/friendly-snippets",
     },
     lazy = false,
     opts = {
       sources = {
         default = {
+          "lsp",
           "path",
           "snippets",
           "buffer",
           "tmux",
-          "lazydev",
-          "lsp",
         },
         providers = {
+          lsp = {
+            score_offset = 10,
+          },
+          snippets = {
+            score_offset = 4,
+          },
+          path = {
+            score_offset = 3,
+          },
+          buffer = {
+            score_offset = 2,
+          },
           tmux = {
             module = "blink-cmp-tmux",
             name = "tmux",
+            score_offset = -10,
           },
         },
       },
@@ -26,6 +39,7 @@ return {
         ["<C-u>"] = { "select_prev", "fallback" },
         ["<C-d>"] = { "select_next", "fallback" },
         ["<C-space>"] = { "show" },
+        ["<CR>"] = { "select_and_accept", "fallback" },
       },
     },
   },
