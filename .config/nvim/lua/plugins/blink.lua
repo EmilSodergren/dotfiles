a0ce258b7dcddb4b09540082fa6bd6a662b4e92b
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2025-09-23 09:16:31 +0200

    Integrate colorful-menu with blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -10,36 +10,76 @@ return {
       sources = {
         default = {
           "lsp",
-          "path",
           "snippets",
           "buffer",
+          "path",
           "tmux",
         },
         providers = {
           lsp = {
             score_offset = 10,
+            name = "lsp",
           },
           snippets = {
             score_offset = 4,
-          },
-          path = {
-            score_offset = 3,
+            name = "snippets",
           },
           buffer = {
             score_offset = 2,
+            name = "buffer",
+          },
+          path = {
+            score_offset = -9,
+            name = "path",
+            min_keyword_length = 2
           },
           tmux = {
             module = "blink-cmp-tmux",
-            name = "tmux",
             score_offset = -10,
+            name = "tmux",
+            min_keyword_length = 2
           },
         },
+      },
+      completion = {
+        menu = {
+          max_height = 15,
+          scrolloff = 2,
+          scrollbar = false,
+          auto_show_delay_ms = 20,
+          draw = {
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end
+              }
+            }
+          }
+        }
       },
       keymap = {
         ["<C-u>"] = { "select_prev", "fallback" },
         ["<C-d>"] = { "select_next", "fallback" },
         ["<C-space>"] = { "show" },
         ["<CR>"] = { "select_and_accept", "fallback" },
+      },
+      cmdline = {
+        enabled = true,
+        keymap = {
+          ["<C-u>"] = { "select_prev", "fallback" },
+          ["<C-d>"] = { "select_next", "fallback" },
+          ["<C-space>"] = { "show" },
+          ["<CR>"] = { "select_and_accept", "fallback" },
+        },
+        completion = {
+          menu = { auto_show = false },
+          list = { selection = { preselect = true } },
+        },
       },
     },
   },
