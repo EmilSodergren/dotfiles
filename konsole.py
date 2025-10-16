0046efcbf03164be08afd79782d2f41222945547
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2025-10-16 15:23:00 +0200

    Make konsole.py decide what to build dep on kernel ver

--- konsole.py
+++ konsole.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 from shutil import rmtree
 from pathlib import Path
 import apt
+import check_dep_version
 
 konsole_install_dir = Path.home() / ".local"
 konsole_dir = Path.home() / "konsole"
@@ -11,15 +12,8 @@ build_dir = konsole_dir / "build"
 apt_cache = apt.Cache()
 nproc = str(cpu_count())
 
-build_tag = "v23.08.5"
-
-parser = ArgumentParser(description='Setup the konsole program')
-packages_for_build = [
-    "cmake",
-    "extra-cmake-modules",
-    "g++",
-    "git",
-    "libicu-dev",
+packages_for_build = ["cmake", "extra-cmake-modules", "g++", "git", "make", "qtchooser", "libicu-dev"]
+packages_for_Qt5 = [
     "libkf5auth-dev",
     "libkf5config-dev",
     "libkf5coreaddons-dev",
@@ -37,15 +31,67 @@ packages_for_build = [
     "libqt5gui5",
     "libqt5qml5",
     "libqt5widgets5",
-    "make",
     "qttools5-dev",
     "qt5-qmake",
     "qtbase5-dev",
     "qtbase5-dev-tools",
-    "qtchooser",
     "qtmultimedia5-dev",
 ]
+packages_for_Qt6 = [
+    "libkf6auth-dev",
+    "libkf6config-dev",
+    "libkf6coreaddons-dev",
+    "libkf6crash-dev",
+    "libkf6declarative-dev",
+    "libkf6i18n-dev",
+    "libkf6kcmutils-dev",
+    "libkf6iconthemes-dev",
+    "libkf6textwidgets-dev",
+    "libkf6dbusaddons-dev",
+    "libkf6kcmutils-dev",
+    "libkf6newstuff-dev",
+    "libkf6notifications-dev",
+    "libkf6notifyconfig-dev",
+    "libkf6package-dev",
+    "libkf6parts-dev",
+    "libkf6pty-dev",
+    "libqt6core6t64",
+    "libqt6gui6",
+    "libqt6qml6",
+    "libqt6widgets6",
+    "qmake6",
+    "qt6-base-dev",
+    "qt6-tools-dev",
+    "qt6-multimedia-dev",
+    "qt6-5compat-dev",
+]
 
+
+def get_dep_packages():
+    match check_dep_version.get_kernel_version_string():
+        case "5.15.0":
+            return packages_for_build + packages_for_Qt5
+        case "6.8.0":
+            return packages_for_build + packages_for_Qt5
+        case "6.17.0":
+            return packages_for_build + packages_for_Qt6
+        case _:
+            raise ValueError("No match for current kernel version")
+
+
+def get_konsole_tag():
+    match check_dep_version.get_kernel_version_string():
+        case "5.15.0":
+            return "v23.08.5"
+        case "6.8.0":
+            return "v23.08.5"
+        case "6.17.0":
+            return "v25.08.5"
+        case _:
+            raise ValueError("No match for current kernel version")
+
+
+parser = ArgumentParser(description='Setup the konsole program')
 parser.add_argument('-b', '--build', action='store_true', help='Download/Update sources and build/install')
 parser.add_argument('-c', '--clean', action='store_true', help='Clean before build')
 args = parser.parse_args()
@@ -63,10 +109,10 @@ if args.clean and build_dir.exists():
 
 if args.build:
     if not konsole_dir.exists():
-        call(["git", "clone", "-b", build_tag, "https://invent.kde.org/utilities/konsole.git", konsole_dir])
+        call(["git", "clone", "-b", get_konsole_tag(), "https://invent.kde.org/utilities/konsole.git", konsole_dir])
     else:
         call(["git", "-C", konsole_dir, "pull"])
-        call(["git", "-C", konsole_dir, "checkout", build_tag])
+        call(["git", "-C", konsole_dir, "checkout", get_konsole_tag()])
 
     build_dir.mkdir(exist_ok=True)
     chdir(build_dir)
