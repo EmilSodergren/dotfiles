d0be528b7d423d9ca6764b33a7124ff0bf13a1b3
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2026-03-19 10:30:47 +0100

    Enable new plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,11 @@
 require('colorscheme.tokyonight')
 
-require('config.keymaps')
-require('config.options')
-
 require('plugins.blink')
+require('plugins.treesitter')
 require('plugins.tmux-vim-navigator')
 require('plugins.colorful-menu')
 require('plugins.lua_ls')
+require('plugins.mini')
 
+require('config.keymaps')
+require('config.options')
