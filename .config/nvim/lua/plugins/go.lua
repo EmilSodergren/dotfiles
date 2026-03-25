fc70cc12bb01c576541853897af5f05467c35b0c
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2026-03-25 12:49:20 +0100

    Formatted and removed in go plugins

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -1,35 +1,34 @@
-vim.pack.add({ 
-  { src = "https://github.com/ray-x/go.nvim", version = "master" },
-  { src = "https://github.com/ray-x/guihua.lua", version = "master" },
+vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig", version = "master" },
 })
+
 vim.lsp.enable("gopls")
 vim.lsp.enable("golangci_lint_ls")
 
 vim.lsp.config("gopls", {
-          cmd = { os.getenv("HOME") .. "/go/bin/gopls" },
-          settings = {
-            gopls = {
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-            },
-          },
-        })
+  cmd = { os.getenv("HOME") .. "/go/bin/gopls" },
+  settings = {
+    gopls = {
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+})
 vim.lsp.config("golangci_lint_ls", {
-          cmd = { os.getenv("HOME") .. "/go/bin/golangci-lint-langserver" },
-          root_dir = function(bufnr, ondir)
-            local fname = vim.api.nvim_buf_get_name(bufnr)
-            ondir(require("lspconfig.util").root_pattern("go.mod", "go.work", ".git")(fname))
-          end,
-          init_options = {
-            command = { "golangci-lint", "run", "--output.json.path=stdout", "--show-stats=false" },
-          },
-          diagnostics = true,
-        })
+  cmd = { os.getenv("HOME") .. "/go/bin/golangci-lint-langserver" },
+  root_dir = function(bufnr, ondir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    ondir(require("lspconfig.util").root_pattern("go.mod", "go.work", ".git")(fname))
+  end,
+  init_options = {
+    command = { "golangci-lint", "run", "--output.json.path=stdout", "--show-stats=false" },
+  },
+  diagnostics = true,
+})
