2c562ac3100de499bd381e224fb79830f6d340bf
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2026-03-25 14:13:10 +0100

    Removed unnecessary treesitter setup

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,3 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = vim.version.range("*") },
-})
-
-require("nvim-treesitter.configs").setup({
-  highlight = { enable = true },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
