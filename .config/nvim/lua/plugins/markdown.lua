fb2bc92f4031911b07e184659892a1ba47cf7584
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2026-03-25 13:32:50 +0100

    Use tags on markdown previen and treesitter

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,14 @@
 vim.pack.add({
-  { src = "https://github.com/iamcco/markdown-preview.nvim",              version = "v0.0.10" },
+  { src = "https://github.com/iamcco/markdown-preview.nvim",              version = vim.version.range("*") },
   { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim", version = "main" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter",           version = "main" },
   { src = "https://github.com/nvim-mini/mini.icons",                      version = "main" },
   { src = "https://github.com/neovim/nvim-lspconfig",                     version = "master" },
 })
 
-require("render-markdown").setup({})
+require("render-markdown").setup({
+  latex = { enabled = false },
+})
 
 vim.lsp.config("marksman", {
   cmd = {
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,7 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = vim.version.range("*") },
+})
+
+require("nvim-treesitter.configs").setup({
+  highlight = { enable = true },
 })
