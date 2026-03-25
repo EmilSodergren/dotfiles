4191de7971de6ace13c598ffb95805a78c6efa4c
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2026-03-25 10:55:27 +0100

    Fix some coloring for git add/delete

--- .config/nvim/lua/colorscheme/tokyonight.lua
+++ .config/nvim/lua/colorscheme/tokyonight.lua
@@ -14,7 +14,6 @@ require("tokyonight").setup({
   on_colors = function(colors)
     colors.yellow = "#ffff8f"
     -- colors.hint = colors.orange
-    -- colors.error = "#ff0000"
   end,
 
   on_highlights = function(hl, c)
@@ -25,14 +24,21 @@ require("tokyonight").setup({
     hl.MiniTrailspace = { bg = "#5f0000", }
     hl.DiffAdd = { bg = "none", fg = "#55EE33" }
     hl.GitSignsAdd = { link = "DiffAdd" }
+    hl.NeogitChangeAdded = { link = "DiffAdd" }
+    hl.NeogitDiffAdd = { link = "DiffAdd" }
+    hl.NeogitDiffAddHighlight = { link = "NeogitDiffAdd" }
     hl.diffNewFile = { link = "DiffAdd" }
     hl.diffAdded = { link = "DiffAdd" }
 
     hl.DiffChange = { bg = "none", fg = "#EEC933" }
     hl.GitSignsChange = { link = "DiffChange" }
+    hl.NeogitChangeUpdated = { link = "DiffChange" }
 
     hl.DiffDelete = { bg = "none", fg = "#EE3333" }
     hl.GitSignsDelete = { link = "DiffDelete" }
+    hl.NeogitChangeDeleted = { link = "DiffDelete" }
+    hl.NeogitDiffDelete = { link = "DiffDelete" }
+    hl.NeogitDiffDeleteHighlight = { link = "DiffDelete" }
     hl.diffRemoved = { link = "DiffDelete" }
     hl.diffOldFile = { link = "DiffDelete" }
   end,
